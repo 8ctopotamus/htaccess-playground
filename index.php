fba4fa878c87c2a6ad7bb3c7a4977dd01b639cfc
Josh <zylo.codes@gmail.com>
2024-12-09 16:03:10 -0600

    capturing path parts

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <h1>Home</h1>
+
+<?php
+
+if (isset($_GET['path'])) {
+  $path = $_GET['path'];
+  $parts = explode('/', $path);
+  foreach ($parts as $index => $part) {
+    echo "Part $index: $part<br>";
+  }
+}
+
+?>
